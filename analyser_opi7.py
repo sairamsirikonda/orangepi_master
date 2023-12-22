fe1503eb894ad4e1deb00c4273aa48f7125959c1
sairamsirikonda <144334867+sairamsirikonda@users.noreply.github.com>
2023-12-22 13:20:26 +0530

    Update analyser_opi7.py

--- analyser_opi7.py
+++ analyser_opi7.py
@@ -1,49 +1,43 @@
 import struct
 import serial
-import logging
 
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
-
-slave_address = 1
+# Configuration
+slave_address = 1  # Change to the correct slave address
 baud_rate = 9600
 serial_port = '/dev/ttyS0'
 
-
+# Function to read a 32-bit float from Modbus address
 def read_float_from_modbus(address, slave):
     try:
-        with serial.Serial(port=serial_port, baudrate=baud_rate, parity=serial.PARITY_NONE,
-                           stopbits=serial.STOPBITS_ONE, timeout=1) as ser:
-            # Construct Modbus command
-            command_to_send = struct.pack('>BBBBHH', slave, 0x03, (address - 1) >> 8, (address - 1) & 0xFF, 0x00, 0x02)
-            
-            # Send command
-            ser.write(command_to_send)
-            
-            # Read response (adjust bytes read based on Modbus data)
-            response = ser.read(7 + 1)  # 7 bytes (header) + 1 byte (byte count)
+        ser = serial.Serial(port=serial_port, baudrate=baud_rate, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1)
 
-            # Log command and response for debugging
-            logging.debug(f"Sent command: {command_to_send}")
-            logging.debug(f"Received response: {response}")
+        # Modbus request: read holding registers (function code 0x03)
+        # Request format: [Slave Address][Function Code][Starting Address Hi][Starting Address Lo][Quantity of Registers Hi][Quantity of Registers Lo][CRC Hi][CRC Lo]
+        command_to_send = struct.pack('>BBBBHH', slave, 0x03, (address - 1) >> 8, (address - 1) & 0xFF, 0x00, 0x02)  # 0x00, 0x02 = read 2 registers (32-bit float)
+        ser.write(command_to_send)
 
-            # Check response length
-            if len(response) >= 8:  # 7 bytes (header) + 1 byte (byte count) + at least 4 bytes for float
-                # Extract float value
-                byte_count = response[2]
-                data_bytes = response[3:3 + byte_count]
-                logging.debug(f"Data bytes: {data_bytes}")
-                value = struct.unpack('>f', data_bytes)[0]
-                return value
-            else:
-                logging.warning("Unexpected response length.")
-                return None
+        # Read response (adjust bytes read based on Modbus data)
+        response = ser.read(7 + 1)  # 7 bytes (header) + 1 byte (byte count)
+
+        # Print the sent command and received response for debugging
+        print(f"Sent command: {command_to_send}")
+        print(f"Received response: {response}")
+
+        # Extracting the float value from the response (assuming it's a big-endian float)
+        if len(response) >= 4:
+            byte_count = response[2]
+            value = struct.unpack('>f', response[3:3 + byte_count])[0]
+            return value
 
     except Exception as e:
-        logging.error(f"Error: {e}")
-        return None
+        print(f"Error: {e}")
+    finally:
+        ser.close()
 
+# Read the 32-bit float value from a range of Modbus addresses
+start_address = 40001
+end_address = 40010  # Adjust to the end address of your data range
 
-# Example usage
-data = read_float_from_modbus(40001, slave_address)
-print("Process Value:", data)
+for address in range(start_address, end_address + 1):
+    data = read_float_from_modbus(address, slave_address)
+    print(f"Process Value at address {address}: {data}")
