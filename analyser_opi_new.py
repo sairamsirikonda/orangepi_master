9fb41e8a91ac5317af712de607a1bd601eb95363
sairam <sairamsirikonda121@gmail.com>
2023-12-26 14:41:49 +0530

    changes

--- analyser_opi_new.py
+++ analyser_opi_new.py
@@ -5,14 +5,13 @@ from opi.gpio import port
 # Open the serial port
 ser = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1)
 
-<<<<<<< HEAD
+
 # Define GPIO pin numbers for DE and RE
 DE_RE_PIN = port.PG7  # Adjust based on your actual GPIO pin configuration
 
-=======
+
 # Choose a GPIO pin for both DE and RE (use appropriate pin names for Orange Pi)
 DE_RE_PIN = port.PG0  # Replace with the appropriate GPIO pin
->>>>>>> be903ae62360f6e80023b7a02b33856c6dcd686a
 
 # Setup GPIO
 gpio.init()
@@ -37,10 +36,8 @@ def send_modbus_request(address):
 
     # Disable transmission (DE and RE high)
     gpio.output(DE_RE_PIN, gpio.HIGH)
-<<<<<<< HEAD
 
-=======
->>>>>>> be903ae62360f6e80023b7a02b33856c6dcd686a
+
 
     # Receive the Modbus response
     response = ser.read(8)  # Adjust the number based on the expected response length
