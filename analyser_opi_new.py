71972818f15956cdee9b25a76fc0166f11bcc969
sairam <sairamsirikonda121@gmail.com>
2023-12-26 13:55:28 +0530

    updates

--- analyser_opi_new.py
+++ analyser_opi_new.py
@@ -6,19 +6,17 @@ from pyA20.gpio import port
 ser = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1)
 
 # Define GPIO pin numbers for DE and RE
-DE_PIN = port.PA6  # Adjust based on your actual GPIO pin configuration
-RE_PIN = port.PA7  # Adjust based on your actual GPIO pin configuration
+DE_RE_PIN = port.PA7  # Adjust based on your actual GPIO pin configuration
 
 # Setup GPIO
 gpio.init()
-gpio.setcfg(DE_PIN, gpio.OUTPUT)
-gpio.setcfg(RE_PIN, gpio.OUTPUT)
+gpio.setcfg(DE_RE_PIN, gpio.OUTPUT)
 
 # Function to send a Modbus request and receive the response
 def send_modbus_request(address):
     # Enable transmission (DE and RE low)
-    gpio.output(DE_PIN, gpio.LOW)
-    gpio.output(RE_PIN, gpio.LOW)
+    gpio.output(DE_RE_PIN, gpio.LOW)
+    
 
     # Construct the Modbus RTU request
     command = bytes([1, 3, 156, address, 0, 0, 0, 2])  # Modify as needed
@@ -33,8 +31,8 @@ def send_modbus_request(address):
     ser.flush()
 
     # Disable transmission (DE and RE high)
-    gpio.output(DE_PIN, gpio.HIGH)
-    gpio.output(RE_PIN, gpio.HIGH)
+    gpio.output(DE_RE_PIN, gpio.HIGH)
+    
 
     # Receive the Modbus response
     response = ser.read(8)  # Adjust the number based on the expected response length
