be3fcd9a153e58d6fceaf752c384bcf2fd9d8ab2
sairamsirikonda <144334867+sairamsirikonda@users.noreply.github.com>
2023-12-29 12:51:50 +0530

    Update co_analyser.py

--- co_analyser.py
+++ co_analyser.py
@@ -2,16 +2,16 @@ import serial
 import time
 
 # Define the serial port parameters
-serial_port = '/dev/ttyS1'  # Update this with the correct serial port for your Orange Pi
+serial_port = '/dev/ttyS0'  # Update this with the correct serial port for your Orange Pi
 baud_rate = 9600
 
 # Create a serial object
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
-def read_co_data():
+def send_custom_command():
     try:
-        # Send the command to request CO data
-        command = bytes.fromhex('7D7B011001F2')
+        # Send the specified command
+        command = bytes.fromhex('F22B550000B1F67D7D7D7B011001')
         ser.write(command)
 
         # Wait for a short time to allow the sensor to respond
@@ -21,7 +21,7 @@ def read_co_data():
         response = ser.readline().decode('utf-8').strip()
 
         # Print the received data
-        print("Received CO data:", response)
+        print("Received response:", response)
 
         # Parse and process the data as needed
         # (Make sure to replace this with your actual data processing logic)
@@ -34,4 +34,4 @@ def read_co_data():
         ser.close()
 
 if __name__ == "__main__":
-    read_co_data()
+    send_custom_command()
