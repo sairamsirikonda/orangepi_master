7cea8b51573f9844d6ca600a273cbfa4116a77b3
sairamsirikonda <144334867+sairamsirikonda@users.noreply.github.com>
2024-01-12 12:18:02 +0530

    Update analyser_opi_new4.py

--- analyser_opi_new4.py
+++ analyser_opi_new4.py
@@ -3,8 +3,8 @@ import struct
 from pyA20.gpio import gpio
 from pyA20.gpio import port
 
-# Open the serial port
-ser = serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1)
+# Open the serial port with no parity
+ser = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1, parity=serial.PARITY_NONE)
 
 # Choose a GPIO pin for both DE and RE (use appropriate pin names for Orange Pi)
 DE_RE_PIN = port.PA7  # Replace with the appropriate GPIO pin
