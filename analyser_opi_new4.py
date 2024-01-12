2bcad66f601a95e4e89a693d2411c077186a64b9
sairamsirikonda <144334867+sairamsirikonda@users.noreply.github.com>
2024-01-12 12:36:54 +0530

    Update analyser_opi_new4.py

--- analyser_opi_new4.py
+++ analyser_opi_new4.py
@@ -1,29 +1,25 @@
-from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+import minimalmodbus
 
 # Open the Modbus connection
-client = ModbusClient(method='rtu', port='/dev/ttyS1', baudrate=9600, timeout=1, parity='N')
-client.connect()
+instrument = minimalmodbus.Instrument('/dev/ttyS0', 1)  # Replace '/dev/ttyS0' with your serial port
 
 try:
     registers = [40001, 40003, 40005]
 
     for register in registers:
         # Example read operation
-        result = client.read_input_registers(register - 1, 2, unit=1)
+        result = instrument.read_registers(register - 1, 2)
 
         # Print the complete Modbus response received
         print(f"Complete response for register {register}: {result}")
 
         # Process the response
-        if result.isError():
-            print(f"Error reading register {register}: {result}")
-        else:
-            values = result.registers
-            print(f"Values at register {register}: {values}")
+        values = result
+        print(f"Values at register {register}: {values}")
 
 except Exception as e:
     print(f"Error: {e}")
 
 finally:
     # Close the Modbus connection
-    client.close()
+    instrument.close()
