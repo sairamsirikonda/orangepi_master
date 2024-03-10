fdc256ea5b776084604b61ed859d0e4fb2700b0c
sairamsirikonda <144334867+sairamsirikonda@users.noreply.github.com>
2024-03-10 12:45:23 +0530

    Update new_pymodbus1.py

--- new_pymodbus1.py
+++ new_pymodbus1.py
@@ -10,7 +10,7 @@ client = ModbusSerialClient(
 )
 if client.connect():  # Trying for connect to Modbus Server/Slave
     '''Reading from a holding register with the below content.'''
-    res = client.read_holding_registers(address=1, count=1, unit=1)
+    res = client.read_holding_registers(address=40003, count=1, unit=1)
     '''Reading from a discrete register with the below content.'''
     # res = client.read_discrete_inputs(address=1, count=1, unit=1)
     if not res.isError():
